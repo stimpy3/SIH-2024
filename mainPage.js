a78f6b13043698b51e89efdaf5b733568661baae
sohan bhadalkar <140563738+stimpy3@users.noreply.github.com>
2024-09-06 07:01:21 +0530

    Update mainPage.js

--- mainPage.js
+++ mainPage.js
@@ -214,4 +214,20 @@ function expirydate(){
     check["exC"]=0;
     }
 }
-//--------------------------------------------------------------------------
+//---------------------------profileName-----------------------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the stored email from localStorage
+    const storedEmail = localStorage.getItem('userEmail');
+
+    // Find the profile name element
+    const nameProfile = document.getElementById('nameProfile');
+
+    if (storedEmail) {
+        // Extract the part before the '@' symbol
+        const username = storedEmail.split('@')[0];
+        nameProfile.textContent = `${username}`;
+    } else {
+        nameProfile.textContent = 'Profile';
+    }
+});
+
