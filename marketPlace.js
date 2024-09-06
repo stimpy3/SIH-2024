63dcba383e3f0566d8a4bb9e721d2ec661e35223
sohan bhadalkar <140563738+stimpy3@users.noreply.github.com>
2024-09-06 07:02:12 +0530

    Update marketPlace.js

---
+++ marketPlace.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the stored email from localStorage
+    const storedEmail = localStorage.getItem('userEmail');
+
+    // Find the profile name element
+    const nameProfile = document.getElementById('nameProfile');
+
+    if (storedEmail) {
+        // Extract the part before the '@' symbol
+        const username = storedEmail.split('@')[0];
+        nameProfile.textContent = `${username}`;
+    } else {
+        nameProfile.textContent = 'Profile';
+    }
+});
